a77f0b8eb4d56d6b57637dc0ff03eb32ce0b4c86
Ahmed Samir <144508508+a7medsr@users.noreply.github.com>
2025-12-26 20:27:57 +0200

    add Container scanning

---
+++ CyberBrief/Controllers/ContainerController.cs
@@ -0,0 +1,57 @@
+﻿using CyberBrief.DTOs.Container;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using CyberBrief.Services;
+namespace CyberBrief.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+        private readonly ContainerServices _containerServices;
+        public ContainerController(ContainerServices containerServices)
+        {
+            _containerServices = containerServices;
+        }
+
+        [HttpPost("scan-image")]
+        public async Task<IActionResult> ScanImage(
+    [FromQuery] string name,
+    [FromQuery] string scanId)
+        {
+            var url =
+                $"https://scannv4.proudforest-e230a1a0.francecentral.azurecontainerapps.io/api/image/{name}/scan/{scanId}/json-report";
+
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode(
+                    (int)response.StatusCode,
+                    "Failed to fetch scan report"
+                );
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var raw = JsonSerializer.Deserialize<RawScanResponse>(
+                jsonString,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            if (raw == null)
+                return BadRequest("Invalid scan response");
+
+            ScanResultDto resultDto = _containerServices.MapToDto(raw);
+
+            return Ok(resultDto);
+        }
+
+    }
+}
---
+++ CyberBrief/Services/ContainerServices.cs
@@ -0,0 +1,37 @@
+﻿using CyberBrief.DTOs.Container;
+
+namespace CyberBrief.Services
+{
+    public class ContainerServices
+    {
+        public  ScanResultDto MapToDto(RawScanResponse raw)
+        {
+            return new ScanResultDto
+            {
+                Id = raw.Summary.Id,
+                Name = raw.Summary.Image.Name,
+                Tag = raw.Summary.Image.Tag,
+                StartedAt = raw.Summary.StartedAt,
+                FinishedAt = raw.Summary.FinishedAt,
+                Counts = new VulnerabilityCountsDto
+                {
+                    Critical = raw.Summary.Counts.Critical,
+                    High = raw.Summary.Counts.High,
+                    Medium = raw.Summary.Counts.Medium,
+                    Low = raw.Summary.Counts.Low,
+                    Total = raw.Summary.Counts.Total
+                },
+                Vulnerabilities = raw.Vulnerabilities
+                    .Select(v => new VulnerabilityDto
+                    {
+                        Package = v.Package,
+                        Vulnerability = v.Vulnerability,
+                        Severity = v.Severity,
+                        Source = v.Source
+                    })
+                    .ToList()
+            };
+        }
+
+    }
+}
